bb624603e7ec9ea6c8611f439e5323e5789650fa
Bravolly <bravolly@gmail.com>
2018-10-30 18:53:49 -0700

    Updates

---
+++ precache-manifest.6edab836eac07c56ae8eb2e15710d7aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "8171d82db746723fa53a",
+    "url": "/RoboFriends/static/js/main.8171d82d.chunk.js"
+  },
+  {
+    "revision": "6372fc1243e037edc36b",
+    "url": "/RoboFriends/static/js/1.6372fc12.chunk.js"
+  },
+  {
+    "revision": "8171d82db746723fa53a",
+    "url": "/RoboFriends/static/css/main.926988ca.chunk.css"
+  },
+  {
+    "revision": "6372fc1243e037edc36b",
+    "url": "/RoboFriends/static/css/1.edfea4b7.chunk.css"
+  },
+  {
+    "revision": "cf369bfceea48c14de8120f19df832b3",
+    "url": "/RoboFriends/index.html"
+  }
+];
