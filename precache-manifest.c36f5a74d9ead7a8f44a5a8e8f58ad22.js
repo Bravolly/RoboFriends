7f9240eadbe55325547d9af7a1e2aa4038d02040
Bravolly <pbravolly@yahoo.com>
2020-03-29 19:46:50 -0700

    Updates

---
+++ precache-manifest.c36f5a74d9ead7a8f44a5a8e8f58ad22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92cc35648420ba251a37d7edd83ac898",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "81135359da75f5ce9987",
+    "url": "/RoboFriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "17bb879bf515b47369a8",
+    "url": "/RoboFriends/static/css/main.8da0ff7c.chunk.css"
+  },
+  {
+    "revision": "81135359da75f5ce9987",
+    "url": "/RoboFriends/static/js/2.8d9b2076.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboFriends/static/js/2.8d9b2076.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17bb879bf515b47369a8",
+    "url": "/RoboFriends/static/js/main.ef534059.chunk.js"
+  },
+  {
+    "revision": "dcfa0c5de83e178aba1b",
+    "url": "/RoboFriends/static/js/runtime-main.641bd6fd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RoboFriends/precache-manifest.254d21af765188f74a9c2944b316831d.js"
+  "/RoboFriends/precache-manifest.c36f5a74d9ead7a8f44a5a8e8f58ad22.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/RoboFriends/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/RoboFriends/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
